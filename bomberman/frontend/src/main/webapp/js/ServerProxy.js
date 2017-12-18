b336b1d1120caa9ca0ac777ef5f622d80b3415d9
bibliophagist <greedthisissad@gmail.com>
2017-12-18 15:58:57 +0300

    finalGame

--- bomberman/frontend/src/main/webapp/js/ServerProxy.js
+++ bomberman/frontend/src/main/webapp/js/ServerProxy.js
@@ -2,6 +2,7 @@ ServerProxy = Class.extend({
     //gameServerUrl: "52.91.84.175:22012",
     //gameServerUrl: "34.229.24.192:22013",
     //gameServerUrl: "192.168.99.100:8090",
+    // gameServerUrl: "34.227.95.123:20021",
     gameServerUrl: "localhost:8090",
     matchMakerUrl: "http://localhost:8080/matchmaker/",
     gameId: "1234",
